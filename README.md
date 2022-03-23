9a5ee5c2656051bfbd0c3dcbb2d619a9366b511c
Colin Ogoo <ogoo.colin@gmail.com>
2022-03-23 20:55:40 +0000

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Svelte web components
+
+Web components with Svelte
+
+## Get started
+
+Install the dependencies
+
+```bash
+## from the root of the project
+npm install
+```
+
+### Building the Web Component
+
+```bash
+npm run build
+```
+
+### Serving the application
+
+```bash
+cd public
+npx servor
+```
+
+
---
+++ src/SvelteWC.svelte
@@ -0,0 +1,23 @@
+<svelte:options tag="svelte-wc" />
+
+<script>
+	export let name = 'world';
+</script>
+
+<h1>Hello {name}!</h1>
+<slot></slot>
+
+<style>
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		h1 {
+            color: #ff6600;
+		}
+	}
+</style>
